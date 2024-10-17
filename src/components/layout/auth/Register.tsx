73066ff295bc50354e80372c54ebd6994fef786c
sadat66 <imteaz.sadat@corexlab.dev>
2024-10-17 18:55:19 +0600

    onboard updated

--- src/components/layout/auth/Register.tsx
+++ src/components/layout/auth/Register.tsx
@@ -13,7 +13,7 @@ import frame3 from "../../../../public/images/on-board/Frame (3).svg";
 import frame4 from "../../../../public/images/on-board/Frame (4).svg";
 import frame5 from "../../../../public/images/on-board/Frame (5).svg";
 import frame6 from "../../../../public/images/on-board/Frame (6).svg";
-import frame7 from "../../../../public/images/on-board/Frame (7).svg";
+
 import { cn } from "@/lib/utils";
 
 
@@ -44,66 +44,101 @@ const stepsConfig: Step[] = [
     questions: [
       { question: "Have children aged 11 years or younger", key: "under_11" },
       {
-        question: "Have children with special care needs",
+        question: "I have children aged 12 or older with special care needs",
         key: "special_needs",
       },
-      { question: "Travel for childcare expenses", key: "child_care_expenses" },
+      { question: "Have additional travel distance or expenses related to dropping off the child in a child day care centre or after-school supervision scheme", key: "child_care_expenses" },
       { question: "I am a single parent", key: "single_parent" },
     ],
     icon: frame,
   },
   {
-    title: "Work and Education",
+    title: "Bank and Loans",
+    description:
+      "This info allows Keeper to suggest tax savings. Select all that apply.",
+    questions: [
+      { question: "Have a loan", key: "moved_for_job" },
+      {
+        question: "Have refinanced a loan in the last year",
+        key: "long_commute",
+      },
+      {
+        question: "Have taken out a joint loan with someone",
+        key: "lived_in_norway",
+      },
+      { question: "Have young people’s housing savings (BSU)", key: "trade_union" },
+
+      { question: "I have sold shares or securities at a loss", key: "sold_shares" }
+    ],
+    icon: frame1,
+  },
+  {
+    title: "Work and education",
     description:
       "This info allows Keeper to suggest tax savings. Select all that apply.",
     questions: [
       { question: "Moved for a new job", key: "moved_for_job" },
       {
-        question: "The return distance between home and work is > 37 km",
+        question: "I work as a fisherman",
         key: "long_commute",
       },
       {
-        question: "I lived in Norway for part of the year",
+        question: "I work as a seafarer",
         key: "lived_in_norway",
       },
-      { question: "Member of a trade union", key: "trade_union" },
+      { question: "The return distance between home and work is more than 37 kilometres", key: "trade_union" },
+      {
+        question: "Have expenses for road toll or ferry when travelling between your home and workplace",
+        key: "lived_in_norway",
+      },
+      {
+        question: "I stay away from home overnight because of work",
+        key: "lived_in_norway",
+      },
     ],
-    icon: frame1,
-  },
-  {
-    title: "Travel",
-    questions: [{ question: "Used ferry/toll for work", key: "ferry_toll" }],
     icon: frame2,
   },
   {
-    title: "Housing",
-    questions: [{ question: "Rent an apartment", key: "rent_apartment" }],
+    title: "Housing and Property",
+    description:
+      "This info allows Keeper to suggest tax savings. Select all that apply.",
+    
+    questions: [{ question: "Housing in a housing association, housing company or jointly owned property", key: "ferry_toll" },
+    { question: "I have rented out a residential property or a holiday home", key: "ferry_toll" },
+    { question: "Sold a residential property or holiday home with a loss", key: "ferry_toll" }
+              ],
+    
     icon: frame3,
   },
   {
-    title: "Income",
-    questions: [{ question: "Received bonuses", key: "received_bonus" }],
+    title: "Gift and donations",
+    description:
+      "This info allows Keeper to suggest tax savings. Select all that apply.",
+    questions: [{ question: "I have made donations to voluntary organisations", key: "rent_apartment" }],
     icon: frame4,
   },
   {
-    title: "Investments",
-    questions: [
-      { question: "Made stock investments", key: "stock_investments" },
+    title: "Hobby, Odd jobs and Extra incomes",
+    description:
+      "This info allows Keeper to suggest tax savings. Select all that apply.",
+    questions: [{ question: "I have a sole proprietorship", key: "received_bonus" },
+      { question: "Sell goods or services, blog/influencer, practise e-sports (gaming), breed animals, or rent out property on a small scale.", key: "received_bonus" },
+      { question: "I have received salary from odd jobs and services", key: "received_bonus" }
     ],
     icon: frame5,
   },
   {
-    title: "Donations",
-    questions: [{ question: "Donated to charity", key: "charity_donations" }],
+    title: "Foreign Income",
+    description:
+      "This info allows Keeper to suggest tax savings. Select all that apply.",
+    questions: [
+      { question: "Have income or wealth in another country than Norway and pay tax in the other country", key: "stock_investments" },
+    ],
     icon: frame6,
   },
+  
 
-  {
-    title: "Signup",
-    description: "Enter your personal information to complete the signup.",
-    isFinalStep: true,
-    icon: frame7,
-  },
+  
 ];
 
 // Multi-step form component
@@ -145,29 +180,25 @@ export default function Register() {
         </TabsList>
 
         {stepsConfig.map((step, index) => (
-          <TabsContent key={index} value={index.toString()} className="w-full border">
+          <TabsContent key={index} value={index.toString()} className="w-full">
             {/* Step title and description */}
 
             <div className="flex flex-col items-center justify-center space-y-1">
             <Image src={step.icon ?? "/images/default-icon.svg"} alt="image" height={30} width={30} />
 
-              <h2 className="text-2xl font-semibold text-center">{step.title}</h2>
+              <h2 className="text-[var(--700,#18181B)] font-inter text-[24px] font-bold leading-normal py-[12px]">{step.title}</h2>
               {step.description && (
-                <p className="text-gray-600 text-center">{step.description}</p>
+                <p className="text-gray-600 text-center py-[12px] text-[var(--500,#71717A)] font-inter text-[12px] font-medium leading-normal">{step.description}</p>
               )}
             </div>
 
 
             {/* Render questions if present */}
             {step.questions?.map((q) => (
-              <label key={q.key} className="flex items-center space-x-3">
-                <input
-                  type="checkbox"
-                  {...register(q.key)}
-                  className="rounded border-gray-300"
-                />
-                <span>{q.question}</span>
-              </label>
+            <label key={q.key} className="flex flex-col justify-center items-center p-4 gap-4 self-stretch rounded-md border border-gray-300 my-2">
+            <span className="self-stretch text-black text-center font-inter text-sm font-normal leading-[150%]">{q.question}</span>
+          </label>
+          
             ))}
 
             {/* Render inputs for final step (signup) */}
