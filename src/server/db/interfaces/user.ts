14eea4d3a800f9e652d772f9664803a362299d1b
zubaerSumon <zubaer@corexlab.dev>
2024-10-20 10:57:12 +0600

    qestionaries added to user model

--- src/server/db/interfaces/user.ts
+++ src/server/db/interfaces/user.ts
@@ -1,8 +1,19 @@
 import { Document } from 'mongoose';
 
+interface IQuestion {
+  questionText: string;  // E.g., 'Moved for a new job'
+  isSelected: boolean;   // If the question is selected
+}
+
+interface IQuestionnaire {
+  category: string;       // E.g., 'Work and Education'
+  questions: IQuestion[]; // Array of questions
+}
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role:string
+  role:string;
+  questionnaires: 
+  
 }
--- src/server/db/models/user.ts
+++ src/server/db/models/user.ts
@@ -1,17 +1,31 @@
 import mongoose, { Schema } from "mongoose";
 import { IUser } from "../interfaces/user";
 
+
+
 const UserSchema: Schema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: {
-        type: String,
-        enum: ["admin", "auditor", "client"],
-        required: true,
+      type: String,
+      enum: ["admin", "auditor", "client"],
+      required: true,
+    },
+    questionnaires: [
+      {
+        category: { type: String, required: true }, // E.g., 'Work and Education'
+        questions: [
+          {
+            questionText: { type: String, required: true }, // E.g., 'Moved for a new job'
+            isSelected: { type: Boolean, default: false }, // If the question is selected
+          },
+        ],
       },
+    ],
   },
+
   { timestamps: true }
 );
 
